5b50ab7c5b5c01384fba9ef3a151f3ea01026cd7
Alex  Qiu <alex@Alexs-Mac-14.local>
2024-05-06 10:50:45 -0400

    fix strawberry plant detection missing files

--- Strawberry_Plant_Detection/detect.py
+++ Strawberry_Plant_Detection/detect.py
@@ -1,57 +1,36 @@
 from ultralytics import YOLO
 from PIL import Image
 import cv2
-import numpy as np
 
-def detect(input_image_name, confidence=0.9):
+def detect(input_image_name):
 
     # Absolute path for model weights on Mac 14 pro 
-    model = YOLO("/Users/alex/Documents/Georgia Tech/GTRI FarmHand/Code/IndoorFarming/Strawberry_Plant_Detection/runs/detect/train20/weights/best.pt")
-    results = model.predict(source=input_image_name, conf=confidence)
+    model = YOLO("/Users/alex/Library/CloudStorage/OneDrive-GeorgiaInstituteofTechnology/Documents/Georgia_Tech/GTRI FarmHand/Code/IndoorFarming/Strawberry_Plant_Detection/runs/detect/train20/weights/best.pt")
+    results = model.predict(source=input_image_name, conf=0.9)
 
-    num_instances = len(results[0])
-    print("Number of instances detected: ", num_instances)
+    num_flowers = len(results[0])
+    print("Number of flowers detected: ", num_flowers)
+
+    # for result in results:
+
+    #     result.plot(conf=True, boxes=True, show=True)
 
     # Process each result
     for result in results:
-
+        # Access the Boxes object
         boxes = result.boxes
-
-        classes = result.names
-        print("Classes: ", classes)
-
-        labels = boxes.cls.numpy()
-        # print("Labels: ", labels)
-
-        num_flowers = np.count_nonzero(labels == 0)
-        num_stamen = np.count_nonzero(labels == 1)
-
-        print("Number of flowers detected: ", num_flowers)
-        print("Number of stamen detected: ", num_stamen)
-
         # Check if there are any detections and boxes.xyxy is not empty
         if boxes is not None and len(boxes.xyxy) > 0:
-
-            frame = cv2.imread(input_image_name) # load image 
-
+            # Load the image once, to draw all detected boxes
+            frame = cv2.imread(input_image_name)
             # Iterate through each detected box
-            for ind, (box, confidence) in enumerate(zip(boxes.xyxy, boxes.conf)):
-
-                x1, y1, x2, y2 = map(int, box) # x1, y1 are top-left corner; x2, y2 are bottom-right corner
-                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3) # draw green rectangle
-                cv2.putText(frame, f'Confidence: {confidence:.2f}', (x1 - 130, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2) # annotate image
-                
-                if labels[ind] == 0:
-                    box_label = "Flower"
-                else:
-                    box_label = "Stamen"
-
-                cv2.putText(frame, box_label, (x1 - 130, y1 + 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+            for box, confidence in zip(boxes.xyxy, boxes.conf):
+                # Drawing the rectangle using coordinates from box
+                x1, y1, x2, y2 = map(int, box)
+                cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+                cv2.putText(frame, f'Confidence: {confidence:.2f}', (x1 - 100, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
 
             # Save the annotated image after drawing all boxes
-            cv2.imwrite('detect_output.png', frame)
+            cv2.imwrite('test1_YOLO_output.png', frame)
 
-        else:
-            print("No flower or stamen detected!")
-
-    return frame, boxes, num_flowers, num_stamen
+    return boxes
--- Strawberry_Plant_Detection/test_yolo_model.py
+++ Strawberry_Plant_Detection/test_yolo_model.py
@@ -1,7 +1,31 @@
 from ultralytics import YOLO
 from PIL import Image
 import cv2
-import numpy as np
-from detect import detect
 
-detect("test1.png")
+model = YOLO("./runs/detect/train20/weights/best.pt")
+results = model.predict(source="test.png", conf=0.9)
+
+num_flowers = len(results[0])
+print("Number of flowers detected: ", num_flowers)
+
+# for result in results:
+
+#     result.plot(conf=True, boxes=True, show=True)
+
+# Process each result
+for result in results:
+    # Access the Boxes object
+    boxes = result.boxes
+    # Check if there are any detections and boxes.xyxy is not empty
+    if boxes is not None and len(boxes.xyxy) > 0:
+        # Load the image once, to draw all detected boxes
+        frame = cv2.imread("test.png")
+        # Iterate through each detected box
+        for box, confidence in zip(boxes.xyxy, boxes.conf):
+            # Drawing the rectangle using coordinates from box
+            x1, y1, x2, y2 = map(int, box)
+            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 3)
+            cv2.putText(frame, f'Confidence: {confidence:.2f}', (x1 - 100, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 255), 2)
+
+        # Save the annotated image after drawing all boxes
+        cv2.imwrite('test1_YOLO_output.png', frame)
